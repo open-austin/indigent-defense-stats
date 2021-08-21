142e2a3eef6e776cbe7ca7835f7166e889dee903
derac <dwolson123@gmail.com>
2021-08-21 18:51:05 -0500

    implement going to the case calendar page if there are case links on the page and we haven't already cached all the cases in this file. This allows us to visit the case page with our session cookie.

--- src/scrape_case_data.py
+++ src/scrape_case_data.py
@@ -4,21 +4,24 @@ from time import sleep
 
 from bs4 import BeautifulSoup
 
-MS_WAIT_PER_REQUEST = 100
+import scrape_calendar_data
+
+MS_WAIT_PER_REQUEST = 200
 
 main_page_url = "http://public.co.hays.tx.us/"
+calendar_page_url = "http://public.co.hays.tx.us/Search.aspx?ID=900&NodeID=100,101,102,103,200,201,202,203,204,6112,400,401,402,403,404,405,406,407,6111,6114&NodeDesc=All%20Courts"
 
-Judicial_Officers = [
-    "visiting_officer",
-    "Boyer_Bruce",
-    "Johnson_Chris",
-    "Robison_Jack",
-    "Sherri_Tibbe",
-    "Henry_Bill",
-    "Steel_Gary",
-    "Updegrove_Robert",
-    "Zelhart_Tacie",
-]
+judicial_officer_to_ID = {
+    "visiting_officer": "37809",
+    "Boyer_Bruce": "39607",
+    "Johnson_Chris": "48277",
+    "Robison_Jack": "6140",
+    "Sherri_Tibbe": "55054",
+    "Henry_Bill": "25322",
+    "Steel_Gary": "6142",
+    "Updegrove_Robert": "38628",
+    "Zelhart_Tacie": "48274",
+}
 
 
 if __name__ == "__main__":
@@ -26,10 +29,15 @@ if __name__ == "__main__":
     session = requests.Session()
     main_page = session.get(main_page_url)
 
+    # May not be necessary, grabbing viewstate for form data
+    calendar_page = session.get(calendar_page_url)
+    soup = BeautifulSoup(calendar_page.text, "html.parser")
+    viewstate_token = soup.find(id="__VIEWSTATE")["value"]
+
     # Data dir setup - added this to gitignore for now, may want to remove later
     if not os.path.exists("data_by_JO"):
         os.mkdir("data_by_JO")
-    for JO_name in Judicial_Officers:
+    for JO_name, JO_id in judicial_officer_to_ID.items():
         print(f"Processing {JO_name}")
         # Make folders if they don't exist
         JO_path = os.path.join("data_by_JO", JO_name)
@@ -44,15 +52,38 @@ if __name__ == "__main__":
 
         for cal_html_file in os.scandir(JO_cal_path):
             if not cal_html_file.is_dir():
-                print(f"Processing", cal_html_file.path)
+                case_date = cal_html_file.name.split(".")[0]
+                print(f"Processing cases from {case_date} for {JO_name}")
                 with open(cal_html_file.path, "r") as file_handle:
                     cal_html_str = file_handle.read()
                 cal_soup = BeautifulSoup(cal_html_str, "html.parser")
                 case_anchors = cal_soup.select('a[href^="CaseDetail"]')
+                if case_anchors:
+                    # Continue with the next cal html file is all cases are cached
+                    if all(
+                        os.path.exists(
+                            os.path.join(
+                                JO_case_path,
+                                f"{case_date} {case_anchor['href'].split('=')[1]}.html",
+                            )
+                        )
+                        for case_anchor in case_anchors
+                    ):
+                        print("All cases are cached for this file.")
+                        continue
+
+                    session.post(
+                        calendar_page_url,
+                        data=scrape_calendar_data.make_form_data(
+                            case_date, JO_id, viewstate_token
+                        ),
+                    )
                 for case_anchor in case_anchors:
                     case_url = main_page_url + case_anchor["href"]
                     case_id = case_url.split("=")[1]
-                    case_html_file_path = os.path.join(JO_case_path, case_id)
+                    case_html_file_path = os.path.join(
+                        JO_case_path, f"{case_date} {case_id}.html"
+                    )
 
                     if not os.path.exists(case_html_file_path):
                         # Make request for the case
