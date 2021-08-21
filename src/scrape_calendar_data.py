139438858d4d507907245725c3f5fab8a9e0a1b4
derac <dwolson123@gmail.com>
2021-08-21 16:59:35 -0500

    minor refactoring

--- src/scrape_calendar_data.py
+++ src/scrape_calendar_data.py
@@ -10,10 +10,11 @@ DAYS_OF_RECORDS = 5 * 365
 TODAY = datetime.datetime.today()
 
 main_page_url = "http://public.co.hays.tx.us/"
+# TODO: Might be able to chop off part of this url, need to test
 calendar_page_url = "http://public.co.hays.tx.us/Search.aspx?ID=900&NodeID=100,101,102,103,200,201,202,203,204,6112,400,401,402,403,404,405,406,407,6111,6114&NodeDesc=All%20Courts"
 
 # TODO: Find out the timespan when each JO presided.
-judicial_officers = {
+Judicial_Officer_to_ID = {
     "visiting_officer": "37809",
     "Boyer_Bruce": "39607",
     "Johnson_Chris": "48277",
@@ -26,7 +27,7 @@ judicial_officers = {
 }
 
 
-def mk_cal_results_form_data(startDate, endDate, jo_id):
+def make_form_data(startDate, endDate, jo_id, viewstate):
     return {
         "__EVENTTARGET": "",
         "__EVENTARGUMENT": "",
@@ -80,55 +81,56 @@ def mk_cal_results_form_data(startDate, endDate, jo_id):
     }
 
 
-# Initial setup for the session
-session = requests.Session()
-main_page = session.get(main_page_url)
-calendar_page = session.get(calendar_page_url)
-soup = BeautifulSoup(calendar_page.text, "html.parser")
-viewstate = soup.find(id="__VIEWSTATE")["value"]
+if __name__ == "__main__":
+    # Initial setup for the session
+    session = requests.Session()
+    main_page = session.get(main_page_url)
+    # May not be necessary, grabbing viewstate for form data
+    calendar_page = session.get(calendar_page_url)
+    soup = BeautifulSoup(calendar_page.text, "html.parser")
+    viewstate_token = soup.find(id="__VIEWSTATE")["value"]
 
-# Data dir setup - added this to gitignore for now, may want to remove later
-if not os.path.exists("data_by_JO"):
-    os.mkdir("data_by_JO")
-for JO_name in judicial_officers.keys():
-    JO_path = os.path.join("data_by_JO", JO_name)
-    JO_cal_path = os.path.join(JO_path, "calendar_html")
-    JO_case_path = os.path.join(JO_path, "case_data")
-    if not os.path.exists(JO_path):
-        os.mkdir(JO_path)
-    if not os.path.exists(JO_cal_path):
-        os.mkdir(JO_cal_path)
-    if not os.path.exists(JO_case_path):
-        os.mkdir(JO_case_path)
+    # Data dir setup - added this to gitignore for now, may want to remove later
+    if not os.path.exists("data_by_JO"):
+        os.mkdir("data_by_JO")
+    for JO_name in Judicial_Officer_to_ID.keys():
+        JO_path = os.path.join("data_by_JO", JO_name)
+        JO_cal_path = os.path.join(JO_path, "calendar_html")
+        if not os.path.exists(JO_path):
+            os.mkdir(JO_path)
+        if not os.path.exists(JO_cal_path):
+            os.mkdir(JO_cal_path)
 
-# Days in the past starting with yesterday.
-for DAY_OFFSET in range(1, DAYS_OF_RECORDS):
-    date_string = datetime.datetime.strftime(
-        TODAY - datetime.timedelta(days=DAY_OFFSET), format="%m/%d/%Y"
-    )
-    file_name = f"{date_string.replace('/','-')}.html"
-    for JO_name, JO_id in judicial_officers.items():
-        cal_data_file_path = os.path.join(
-            "data_by_JO", JO_name, "calendar_html", file_name
+    # Days in the past starting with yesterday.
+    for DAY_OFFSET in range(1, DAYS_OF_RECORDS):
+        date_string = datetime.datetime.strftime(
+            TODAY - datetime.timedelta(days=DAY_OFFSET), format="%m/%d/%Y"
         )
-        # Check if the file is already cached before requesting
-        print(f"Capturing data for JO: {JO_name} on {date_string}")
-        if not os.path.exists(cal_data_file_path):
-            form_data = mk_cal_results_form_data(date_string, date_string, JO_id)
-            cal_results = session.post(calendar_page_url, data=form_data)
-            # Error check based on text in html result.
-            if "Record Count" in cal_results.text:
-                print(f"Writing file: {cal_data_file_path}")
-                with open(cal_data_file_path, "w") as file_handle:
-                    file_handle.write(cal_results.text)
-                # Rate limiting - convert ms to seconds
-                sleep(MS_WAIT_PER_REQUEST / 1000)
-            else:
-                print(
-                    f'ERROR: "Record Count" substring not found in html page. Aborting. Writing ./debug.html'
+        file_name = f"{date_string.replace('/','-')}.html"
+        for JO_name, JO_id in Judicial_Officer_to_ID.items():
+            cal_data_file_path = os.path.join(
+                "data_by_JO", JO_name, "calendar_html", file_name
+            )
+            # Check if the file is already cached before requesting
+            print(f"Capturing data for JO: {JO_name} on {date_string}")
+            if not os.path.exists(cal_data_file_path):
+                form_data = make_form_data(
+                    date_string, date_string, JO_id, viewstate_token
                 )
-                with open("debug.html", "w") as file_handle:
-                    file_handle.write(cal_results.text)
-                quit()
-        else:
-            print("Data is already cached. Skipping.")
+                cal_results = session.post(calendar_page_url, data=form_data)
+                # Error check based on text in html result.
+                if "Record Count" in cal_results.text:
+                    print(f"Writing file: {cal_data_file_path}")
+                    with open(cal_data_file_path, "w") as file_handle:
+                        file_handle.write(cal_results.text)
+                    # Rate limiting - convert ms to seconds
+                    sleep(MS_WAIT_PER_REQUEST / 1000)
+                else:
+                    print(
+                        f'ERROR: "Record Count" substring not found in html page. Aborting. Writing ./debug.html'
+                    )
+                    with open("debug.html", "w") as file_handle:
+                        file_handle.write(cal_results.text)
+                    quit()
+            else:
+                print("Data is already cached. Skipping.")
