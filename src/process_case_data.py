92a26241e310780c4808bd1adf9728715c7e046a
derac <dwolson123@gmail.com>
2021-08-22 02:10:00 -0500

    refactor, fix minor issues

--- src/process_case_data.py
+++ src/process_case_data.py
@@ -14,21 +14,19 @@ for JO_folder in os.scandir("data_by_JO"):
         with open(case_html_file.path, "r") as file_handle:
             case_html = file_handle.read()
         case_soup = BeautifulSoup(case_html, "html.parser")
-        # TODO: there are multiple types of cases, most are the CR-* type, but some are different
-        # have a different code naming convention and layout
         # Gather initial data for filename and date checking
         case_data["code"] = case_soup.select('div[class="ssCaseDetailCaseNbr"] > span')[
             0
         ].text
         case_data["osyssey id"] = case_html_file.name.split()[1].split(".")[0]
-        case_data["date"] = case_html_file.name.split()[0]
+        case_data["date"] = case_html_file.name.split()[0].replace("-", "/")
         case_filename = os.path.join(case_data_path, case_data["code"] + ".json")
         # If file exists, check if the cached version has a newer date, if so continue.
         if os.path.exists(case_filename):
             with open(case_filename, "r") as file_handle:
                 cached_data = json.loads(file_handle.read())
-            cached_date = datetime.strptime(cached_data["date"], "%m-%d-%Y")
-            current_date = datetime.strptime(case_data["date"], "%m-%d-%Y")
+            cached_date = datetime.strptime(cached_data["date"], "%m/%d/%Y")
+            current_date = datetime.strptime(case_data["date"], "%m/%d/%Y")
             if cached_date > current_date:
                 print("Cached data is newer. Continuing.")
                 continue
@@ -121,7 +119,7 @@ for JO_folder in os.scandir("data_by_JO"):
                     "prosecuting attorney": state_rows[0][2]
                     if len(state_rows[0]) > 2
                     else "",
-                    "prosecuting attorney phone number": state_rows[0][2]
+                    "prosecuting attorney phone number": state_rows[0][3]
                     if len(state_rows[0]) > 3
                     else "",
                     "prosecuting attorney address": ", ".join(state_rows[1]),
@@ -215,7 +213,6 @@ for JO_folder in os.scandir("data_by_JO"):
 
         print(case_data)
 
-        # Quit for now so we don't write a bunch of crap
         # Write file as json data
         # with open(case_filename, "w") as file_handle:
         #     file_handle.write(json.dumps(case_data))
