d48f4cac39f179e1364598755eac02ed26ddc342
derac <dwolson123@gmail.com>
2021-08-22 06:18:56 -0500

    improve stats

--- src/print_case_stats.py
+++ src/print_case_stats.py
@@ -12,37 +12,46 @@ for JO_dir in os.scandir("data_by_JO"):
 
 
 def print_top_cases_by_lambda(sort_function, description):
-    print("\n", N_LONGEST, "longest cases by", description + ".")
+    print("\n", description)
     cases_by_lambda = sorted(case_data_list, key=sort_function)[-N_LONGEST:]
     print(
         "\n".join(
-            f"{i}. {sort_function(case)} - {case['filename']}"
+            f"{i}. {sort_function(case)}".ljust(20) + case["filename"]
             for i, case in enumerate(cases_by_lambda[::-1], 1)
         )
     )
+    print(
+        "Average:",
+        round(
+            sum(sort_function(case) for case in case_data_list) / len(case_data_list), 2
+        ),
+    )
 
 
-print_top_cases_by_lambda(lambda case: len(str(case)), "string length")
-print(
-    "Average string length of cases:",
-    int(sum(len(str(case)) for case in case_data_list) / len(case_data_list)),
-)
 events_len = lambda case: len(case["other events and hearings"])
 print_top_cases_by_lambda(
     events_len,
-    "'other events and hearings' length",
-)
-print(
-    "Average 'other events and hearings' length of cases:",
-    int(sum(events_len(case) for case in case_data_list) / len(case_data_list)),
+    "other events and hearings length",
 )
 disposition_len = lambda case: len(case["dispositions"])
 print_top_cases_by_lambda(
     disposition_len,
-    "'dispositions' length",
+    "dispositions length",
 )
-print(
-    "Average 'dispositions' length of cases:",
-    int(sum(disposition_len(case) for case in case_data_list) / len(case_data_list)),
+case_cost = (
+    lambda case: float(
+        case["financial information"]["total financial assessment"].replace(",", "")
+    )
+    if "financial information" in case
+    else 0.0
+)
+print_top_cases_by_lambda(
+    case_cost,
+    "highest cost",
+)
+charges_len = lambda case: len(case["charge information"])
+print_top_cases_by_lambda(
+    charges_len,
+    "number of charges",
 )
 print("\nNumber of cases:", len(case_data_list))
