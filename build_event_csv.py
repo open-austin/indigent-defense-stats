d7febff47ccc3f049f7c27329be612416dc43a94
normaljosh <normaljosh@gmail.com>
2022-02-06 18:01:01 -0600

    add charges file to csv, additional appointed or retained cases to parser

--- build_event_csv.py
+++ build_event_csv.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import json
 import os
 
-FILE_DIR = "data"  # relative location of JSON case files
+FILE_DIR = os.path.join("data", "case_json")  # relative location of JSON case files
 
 
 def parse_event_date(date_str):
@@ -29,17 +29,29 @@ def get_days_elapsed(start, end):
 def main():
     files = [file for file in os.listdir(FILE_DIR) if file.endswith(".json")]
     events = []
+    charges = []
 
-    for f in files:
-        with open(f"{FILE_DIR}/{f}", "r") as fin:
+    for f_count, f_name in enumerate(files):
+        if f_count % 1000 == 0:
+            print(f"Processing file {f_count} of {len(files)}")
+
+        with open(f"{FILE_DIR}/{f_name}", "r") as fin:
             """
             Extract fields of interest. you can add any attributes of interest to the
-            event_record dict and they will be included in the output CSV
+            event_record dict and they will be included in the output CSV.
+            Extracts events and charges from the case file, in seperate files.
             """
             case = json.load(fin)
-            retained = case["party information"]["appointed or retained"]
+
+            # extract demographic info
             case_id = case["odyssey id"]
             case_number = case["code"]
+            retained = case["party information"]["appointed or retained"]
+            gender = case["party information"]["sex"]
+            race = case["party information"]["race"]
+            defense_attorney = case["party information"]["defense attorney"]
+
+            # extract event data
             first_event_date = None
             for i, event in enumerate(case["other events and hearings"]):
                 event_record = {}
@@ -57,12 +69,38 @@ def main():
                 event_record["attorney"] = retained
                 event_record["case_id"] = case_id
                 event_record["case_number"] = case_number
+                event_record["defense_attorney"] = defense_attorney
+                event_record["race"] = race
+                event_record["gender"] = gender
                 events.append(event_record)
 
-    with open("events_combined.csv", "w") as fout:
+            # extract charge data
+            for i, charge in enumerate(case["charge information"]):
+                charge_record = {}
+                charge_record["charge_id"] = i + 1
+                charge_record["charge_name"] = charge.get("charges", "")
+                charge_record["statute"] = charge.get("statute", "")
+                charge_record["level"] = charge.get("level", "")
+
+                charge_record["charge_date"] = charge.get("date", "")
+                if charge_record["charge_date"]:
+                    charge_record["charge_date"] = iso_event_date(
+                        parse_event_date(charge_record["charge_date"])
+                    )
+
+                charge_record["case_id"] = case_id
+                charge_record["case_number"] = case_number
+                charges.append(charge_record)
+
+    with open("events_combined.csv", "w", newline="") as fout:
         writer = csv.DictWriter(fout, fieldnames=events[0].keys())
         writer.writeheader()
         writer.writerows(events)
+
+    with open("charges_combined.csv", "w", newline="") as fout:
+        writer = csv.DictWriter(fout, fieldnames=charges[0].keys())
+        writer.writeheader()
+        writer.writerows(charges)
 
 
 if __name__ == "__main__":
