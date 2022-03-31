db021ccf4fea07c3583fdf2cf2f32c03e34080a2
derac <dwolson123@gmail.com>
2022-03-31 05:51:26 -0500

    - logger was not writing to command line, fixed this issue. - fixed issue with request_page usage, caused script to crash since a variable was called that wasn't initialized. - added a parameter to change logger setting, i.e. "-log=DEBUG"

--- src/scraper.py
+++ src/scraper.py
@@ -172,9 +172,18 @@ def main() -> None:
         action="store_true",
         help="Switch to overwrite cached case html, use this when updating your data from a small date range to grab new information.",
     )
+    argparser.add_argument(
+        "-log",
+        type=str,
+        default="INFO",
+        help="Set the level to log at.",
+    )
     argparser.description = "Scrape data for list of judicial officers in date range."
 
     args = argparser.parse_args()
+
+    # set log level
+    logging.root.setLevel(level=args.log)
 
     # make cache directories if not present
     case_html_path = os.path.join("data", "case_html")
@@ -300,18 +309,18 @@ def main() -> None:
 
                     # make request for the case
                     logger.info("Visiting:", case_url)
-                    case_text, failed = request_page(session, case_url, "Date Filed")
+                    case_results, failed = request_page(session, case_url, "Date Filed")
                     # error check based on text in html result.
                     if not failed:
-                        logger.info(f"Response string length: {len(case_results.text)}")
+                        logger.info(f"Response string length: {len(case_results)}")
                         with open(case_html_file_path, "w") as file_handle:
-                            file_handle.write(case_results.text)
+                            file_handle.write(case_results)
                         # add case id to cached list
                         if case_id not in cached_case_html_list:
                             cached_case_html_list.append(case_id)
                     else:
                         curr_vars = f"{day_offset = }\n{date_string = }\n{JO_name = }\n{case_url = }"
-                        write_debug_and_quit("Date Filed", case_results.text, curr_vars)
+                        write_debug_and_quit("Date Filed", case_results, curr_vars)
                     # rate limiting - convert ms to seconds
                     sleep(args.ms_wait / 1000)
 
