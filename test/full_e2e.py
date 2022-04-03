0ede1a97f3e4909c3de456049b3290fbd62bd85f
derac <dwolson123@gmail.com>
2022-04-03 00:23:03 -0500

    abstract testing code

--- test/full_e2e.py
+++ test/full_e2e.py
@@ -2,31 +2,37 @@ from subprocess import check_output, STDOUT
 from unittest import main, TestCase
 
 
+def test_scraper(self, county: str, date: str, judicial_officer: str):
+    output = check_output(
+        [
+            "poetry",
+            "run",
+            "python",
+            "./src/scraper",
+            "-start_date",
+            date,
+            "-end_date",
+            date,
+            "-county",
+            county,
+            "-judicial_officers",
+            judicial_officer,
+            "-test",
+        ],
+        stderr=STDOUT,
+        shell=True,
+    )
+    self.assertIn(
+        b"Testing, stopping after first case",
+        output,
+        "Assert that testing message is logged after first case is scraped successfully.",
+    )
+
+
 class TestScrapingSites(TestCase):
     def test_hays(self):
-        output = check_output(
-            [
-                "poetry",
-                "run",
-                "python",
-                "./src/scraper",
-                "-start_date",
-                "2022-03-22",
-                "-end_date",
-                "2022-03-22",
-                "-county",
-                "hays",
-                "-judicial_officers",
-                "Updegrove, Robert",
-                "-test",
-            ],
-            stderr=STDOUT,
-            shell=True,
-        )
-        self.assertIn(
-            b"Testing, stopping after first case",
-            output,
-            "Assert that testing message is logged after first case is scraped successfully.",
+        test_scraper(
+            self, county="hays", date="2022-03-22", judicial_officer="Updegrove, Robert"
         )
 
 
