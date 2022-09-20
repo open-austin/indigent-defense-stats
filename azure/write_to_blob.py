7c5399886e0fff93c553a66ca6f6b27137a87dd6
normaljosh <normaljosh@gmail.com>
2022-09-20 16:54:19 -0500

    add azure packages to poetry and examples to .py

--- azure/write_to_blob.py
+++ azure/write_to_blob.py
@@ -1,16 +1,26 @@
-# pip install azure-storage-blob
-# pip install azure-identity
-from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import BlobServiceClient, ContainerClient
 from azure.identity import DefaultAzureCredential
 
+# Authenticate to client
 token_credential = DefaultAzureCredential()
-
 blob_service_client = BlobServiceClient(
     account_url="https://indigentdefensepublic.blob.core.windows.net/",
     credential=token_credential,
 )
 
+# Write to blob
 blob_client = blob_service_client.get_blob_client(
     container="indigentdefensepublicblob", blob="test_write.txt"
 )
 blob_client.upload_blob("hello world")
+
+# Read from blob
+data = blob_client.download_blob()
+print(data.readall())
+
+# List blobs
+container_client = blob_service_client.get_container_client(
+    container="indigentdefensepublicblob"
+)
+blob_list = container_client.list_blobs()
+print(blob_list)
