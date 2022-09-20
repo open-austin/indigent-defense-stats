0853f6489d4126eb25d5d14a43a8a9f6c262b6cb
normaljosh <normaljosh@gmail.com>
2022-09-20 14:40:37 -0500

    container had blob in name

--- azure/write_to_blob.py
+++ azure/write_to_blob.py
@@ -9,11 +9,8 @@ blob_service_client = BlobServiceClient(
     account_url="https://indigentdefensepublic.blob.core.windows.net/",
     credential=token_credential,
 )
-all_containers = blob_service_client.list_containers(include_metadata=True)
-for container in all_containers:
-    print(container["name"], container["metadata"])
 
 blob_client = blob_service_client.get_blob_client(
-    container="indigentdefensepublic", blob="test_write.txt"
+    container="indigentdefensepublicblob", blob="test_write.txt"
 )
 blob_client.upload_blob("hello world")
