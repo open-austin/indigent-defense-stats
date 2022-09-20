4e01a626f7583cdaaeb247da5c6637ccdfccfe21
normaljosh <normaljosh@gmail.com>
2022-09-20 14:35:04 -0500

    try AD credential

--- azure/write_to_blob.py
+++ azure/write_to_blob.py
@@ -1,8 +1,12 @@
 # pip install azure-storage-blob
 from azure.storage.blob import BlobServiceClient
+from azure.identity import DefaultAzureCredential
+
+token_credential = DefaultAzureCredential()
 
 blob_service_client = BlobServiceClient(
-    account_url="https://indigentdefensepublic.blob.core.windows.net/"
+    account_url="https://indigentdefensepublic.blob.core.windows.net/",
+    credential=token_credential,
 )
 blob_client = blob_service_client.get_blob_client(
     container="indigentdefensepublic", blob="test_write.txt"
