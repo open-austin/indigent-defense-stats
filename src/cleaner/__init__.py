ca6d227fb5c37aadd62ba24b6660fa9f72b871e1
Jason Lin <jzlin15@gmail.com>
2024-09-26 17:32:22 -0500

    changed back to instance methods & additional updates

--- src/cleaner/__init__.py
+++ src/cleaner/__init__.py
@@ -2,12 +2,12 @@ import json
 import os
 import datetime as dt
 import xxhash
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class Cleaner:
-
-    # commented out for now and maybe remove as it may not be needed anymore? 
-    # def __init__(self, county):
-        # self.county = county.lower()
 
     GOOD_MOTIONS = [
         "Motion To Suppress",
@@ -19,63 +19,64 @@ class Cleaner:
         "Motion In Limine",
     ]
 
-    @staticmethod
-    def add_parsing_date(input_dict: dict, output_json_data: dict) -> dict:
-        # This will add the date of parsing to the final cleaned json file
-        today_date = dt.datetime.today().strftime('%Y-%m-%d')
-        output_json_data['parsing_date'] = today_date
-        return output_json_data
-    
-    @staticmethod
-    def get_folder_path(county, folder_type):
-        """Returns the path for the specified folder type ('case_json' or 'case_json_cleaned')."""
-        return os.path.join(os.path.dirname(__file__), "..", "..", "data", county.lower(), folder_type)
+    def __init__(self):
+        pass
 
-    @staticmethod
-    def ensure_folder_exists(folder_path):
-        """Checks if the output folder exists and creates it if it doesn't"""
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-            print(f"Folder '{folder_path}' created successfully.")
-        else:
-            print(f"Folder '{folder_path}' already exists.")
+    def add_parsing_date(self, output_json_data: dict) -> dict:
+        """Adds the date of parsing to the final cleaned json file"""
+        output_json_data['parsing_date'] = dt.datetime.today().strftime('%Y-%m-%d')
+        return output_json_data 
 
-    @staticmethod
-    def get_and_ensure_folder(county, folder_type):
-        """Gets the folder path and ensures that the folder exists as this folder will contain the cleaned json files."""
-        folder_path = Cleaner.get_folder_path(county, folder_type)
-        Cleaner.ensure_folder_exists(folder_path)
+    def get_or_create_folder_path(self, county: str, folder_type: str) -> str:
+        """Returns and ensures the existence of the folder path."""
+        folder_path = os.path.join(os.path.dirname(__file__), "..", "..", "data", county.lower(), folder_type)
+        try:
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+                logging.info(f"Folder '{folder_path}' created successfully.")
+            else:
+                logging.info(f"Folder '{folder_path}' already exists.")
+        except OSError as e:
+            logging.error(f"Error creating folder '{folder_path}': {e}")
+        return folder_path
 
-    @staticmethod
-    def load_json_file(file_path):
+    def load_json_file(self, file_path: str) -> dict:
         """Loads a JSON file from a given file path and returns the data as an object"""
-        with open(file_path, "r") as f:
-            return json.load(f)
+        try:
+            with open(file_path, "r") as f:
+                return json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError) as e:
+            logging.error(f"Error loading file at {file_path}: {e}")
+            return {}
 
-    @staticmethod
-    def map_charge_names(charge_data):
+    def map_charge_names(self, charge_data: list[dict]) -> dict:
         """Creates a dictionary mapping charge names to their corresponding UMich data"""
-        charge_mapping = {}
-        for item in charge_data:
-            charge_mapping[item['charge_name']] = item
+        return {item['charge_name']: item for item in charge_data}
+
+    def load_and_map_charge_names(self, file_path: str) -> dict:
+        """Loads a JSON file and maps charge names to their corresponding UMich data."""
+        charge_data = self.load_json_file(file_path)
+        # Check if the file loaded successfully
+        if not charge_data:
+            logging.error(f"Failed to load charge data from {file_path}")
+            raise FileNotFoundError(f"File not found or is empty: {file_path}")
+        # Create charge mapping from data
+        try:
+            charge_mapping = self.map_charge_names(charge_data)
+        except KeyError as e:
+            logging.error(f"Error in mapping charge names: {e}")
+            raise ValueError(f"Invalid data structure: {file_path}")
         return charge_mapping
 
-    @staticmethod
-    def load_and_map_charge_names(file_path):
-        """Loads a JSON file and maps charge names to their corresponding UMich data."""
-        charge_data = Cleaner.load_json_file(file_path)
-        return Cleaner.map_charge_names(charge_data)
 
-
-    @staticmethod
-    def process_charges(charges, charge_mapping):
+    def process_charges(self, charges: list[dict], charge_mapping: dict) -> tuple[list[dict], str]:
         """
         Processes a list of charges by formatting charge details, 
         mapping charges to UMich data, and finding the earliest charge date.
     
         Args:
-            charges (list): A list of charges where each charge is a dictionary containing charge details.
-            charge_mapping (dict): A dictionary mapping charge names to corresponding UMich data.
+            charges: A list of charges where each charge is a dictionary containing charge details.
+            charge_mapping: A dictionary mapping charge names to corresponding UMich data.
         
         Returns:
             tuple: A list of processed charges and the earliest charge date.
@@ -93,57 +94,69 @@ class Cleaner:
             }
 
             # Parse the charge date and append it to charge_dates
-            charge_datetime = dt.datetime.strptime(charge["date"], "%m/%d/%Y")
-            charge_dates.append(charge_datetime)
-            charge_dict["charge_date"] = dt.datetime.strftime(charge_datetime, "%Y-%m-%d")
+            try:
+                charge_datetime = dt.datetime.strptime(charge["date"], "%m/%d/%Y")
+                charge_dates.append(charge_datetime)
+                charge_dict["charge_date"] = dt.datetime.strftime(charge_datetime, "%Y-%m-%d")
+            except ValueError:
+                logging.error(f"Error parsing date for charge: {charge}")
+                continue
 
             # Try to map the charge to UMich data
             try:
                 charge_dict.update(charge_mapping[charge["charges"]])
             except KeyError:
-                print(f"Couldn't find this charge: {charge['charges']}")
-                pass
+                logging.warning(f"Couldn't find this charge: {charge['charges']}")
+                continue
 
             processed_charges.append(charge_dict)
 
         # Find the earliest charge date
-        earliest_charge_date = dt.datetime.strftime(min(charge_dates), "%Y-%m-%d")
+        if charge_dates:
+            earliest_charge_date = dt.datetime.strftime(min(charge_dates), "%Y-%m-%d")
+        else:
+            logging.warning("No valid charge dates found.")
+            earliest_charge_date = ""
 
         return processed_charges, earliest_charge_date
 
-    @staticmethod
-    def contains_good_motion(motion, event):
+    def contains_good_motion(self, motion: str, event: list | str) -> bool:
         """Recursively check if a motion exists in an event list or sublist."""
         if isinstance(event, list):
-            return any(Cleaner.contains_good_motion(motion, item) for item in event) 
+            return any(self.contains_good_motion(motion, item) for item in event) 
         return motion.lower() in event.lower()
 
-    @staticmethod
-    def find_good_motions(events, good_motions):
+    def find_good_motions(self, events: list | str, good_motions: list[str]) -> list[str]:
         """Finds motions in events based on list of good motions."""
-        motions_in_events = [
-            motion for motion in good_motions if Cleaner.contains_good_motion(motion, events)
-        ]
-        return motions_in_events
+        return [motion for motion in good_motions if self.contains_good_motion(motion, events)]        
 
-    @staticmethod
-    def hash_defense_attorney(input_dict):
+    def hash_defense_attorney(self, input_dict: dict) -> str:
         """Hashes the defense attorney info to anonymize it."""
-        def_atty_unique_str = f'{input_dict["party information"]["defense attorney"]}:{input_dict["party information"]["defense attorney phone number"]}'
-        return xxhash.xxh64(def_atty_unique_str).hexdigest()
+        try:
+            def_atty_unique_str = f'{input_dict["party information"]["defense attorney"]}:{input_dict["party information"]["defense attorney phone number"]}'
+            return xxhash.xxh64(def_atty_unique_str).hexdigest()
+        except KeyError as e:
+            logging.error(f"Missing defense attorney data: {e}")
+            return ""
 
 
-    @staticmethod
-    def write_json_output(file_path, data):
+    def write_json_output(self, file_path: str, data: dict) -> None:
         """Writes the given data to a JSON file at the specified file path."""
-        with open(file_path, "w") as f:
-            json.dump(data, f)
+        try:
+            with open(file_path, "w") as f:
+                json.dump(data, f)
+            logging.info(f"Successfully wrote cleaned data to {file_path}")
+        except OSError as e:
+            logging.error(f"Failed to write JSON output to {file_path}: {e}")
 
-    @staticmethod
-    def process_single_case(county, case_json_folder_path, case_json_filename):
+    def process_single_case(self, county: str, case_json_folder_path: str, case_json_filename:str) -> None:
         """Process a single case JSON file."""
         input_json_path = os.path.join(case_json_folder_path, case_json_filename)
-        input_dict = Cleaner.load_json_file(input_json_path)
+        input_dict = self.load_json_file(input_json_path)
+
+        if not input_dict:
+            logging.error(f"Failed to load case data from {input_json_path}")
+            return
 
         # Initialize cleaned output data
         output_json_data = {
@@ -155,42 +168,48 @@ class Cleaner:
             "earliest_charge_date": "",
             "motions": [],
             "has_evidence_of_representation": False,
-            "defense_attorney": Cleaner.hash_defense_attorney(input_dict)
+            "defense_attorney": self.hash_defense_attorney(input_dict)
         }
 
         # Load charge mappings
         charge_name_to_umich_file = os.path.join(
             os.path.dirname(__file__), "..", "..", "resources", "umich-uccs-database.json"
         )
-        charges_mapped = Cleaner.load_and_map_charge_names(charge_name_to_umich_file)
+        charges_mapped = self.load_and_map_charge_names(charge_name_to_umich_file)
 
         # Process charges and motions
-        output_json_data["charges"], output_json_data["earliest_charge_date"] = Cleaner.process_charges(
+        output_json_data["charges"], output_json_data["earliest_charge_date"] = self.process_charges(
             input_dict["charge information"], charges_mapped
         )
-        output_json_data["motions"] = Cleaner.find_good_motions(
-            input_dict["other events and hearings"], Cleaner.GOOD_MOTIONS
+        output_json_data["motions"] = self.find_good_motions(
+            input_dict["other events and hearings"], self.GOOD_MOTIONS
         )
         output_json_data["has_evidence_of_representation"] = len(output_json_data["motions"]) > 0
 
         # Add parsing date
-        output_json_data = Cleaner.add_parsing_date(input_dict, output_json_data)
+        output_json_data = self.add_parsing_date(output_json_data)
+
+        # Ensure the case_json_cleaned folder exists
+        cleaned_folder_path = self.get_or_create_folder_path(county, "case_json_cleaned")
 
         # Write output to file
-        output_filepath = os.path.join(
-            os.path.dirname(__file__), "..", "..", "data", county.lower(), "case_json_cleaned", case_json_filename
-        )
-        Cleaner.write_json_output(output_filepath, output_json_data)
+        output_filepath = os.path.join(cleaned_folder_path, case_json_filename)
+        self.write_json_output(output_filepath, output_json_data)
 
-    @staticmethod
-    def process_json_files(county, case_json_folder_path):
+    def process_json_files(self, county: str, case_json_folder_path: str) -> None:
         """Processes all JSON files in the specified folder."""
-        list_case_json_files = os.listdir(case_json_folder_path)
+        try:
+            list_case_json_files = os.listdir(case_json_folder_path)
+        except (FileNotFoundError, Exception) as e:
+            logging.error(f"Error reading directory {case_json_folder_path}: {e}")
+            return
         for case_json_filename in list_case_json_files:
-            Cleaner.process_single_case(county, case_json_folder_path, case_json_filename)
+            try:
+                self.process_single_case(county, case_json_folder_path, case_json_filename)
+            except Exception as e:
+                logging.error(f"Error processing file {case_json_filename}. Error: {e}")
 
-    @staticmethod
-    def clean(county):
+    def clean(self, county: str) -> None:
         """
         Cleans and processes case data for a given county.
         This method performs the following steps:
@@ -201,6 +220,10 @@ class Cleaner:
         5. Adds metadata, such as parsing date and case number, to the cleaned data.
         6. Writes the cleaned data to the 'case_json_cleaned' folder for the specified county.
         """
-        case_json_folder_path = Cleaner.get_folder_path(county, "case_json")
-        Cleaner.get_and_ensure_folder(county, "case_json_cleaned")
-        Cleaner.process_json_files(county, case_json_folder_path)
+        try:
+            case_json_folder_path = self.get_or_create_folder_path(county, "case_json")
+            logging.info(f"Processing data for county: {county}")
+            self.process_json_files(county, case_json_folder_path)
+            logging.info(f"Completed processing for county: {county}")
+        except Exception as e:
+            logging.error(f"Error during cleaning process for county: {county}. Error: {e}")
