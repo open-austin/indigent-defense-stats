2898602d81afe9056dda83439b3f2f4d66ee2e65
KI JONG BYUN <kjbyun0@gmail.com>
2024-10-21 21:14:05 -0500

    Add all loggings to a separate file

--- src/updater/__init__.py
+++ src/updater/__init__.py
@@ -8,21 +8,33 @@ class Updater():
     def __init__(self, county):
         self.county = county.lower()
 
-    def configure_logger(self):
+    def configure_logger(self, dir_path):
         logger = logging.getLogger(name="pid: " + str(os.getpid()))
-        logging.basicConfig()
-        logging.root.setLevel(level="INFO")
+        logger.setLevel(logging.DEBUG)
 
-        file_handler = logging.FileHandler('logger_log.txt')
+        file_handler = logging.FileHandler(os.path.join(dir_path, 'logger_log.txt'))
         file_handler.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
 
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.WARNING)
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+
         return logger
 
     def update(self):
-        logger = self.configure_logger()
+        # open or create a output directory for a log and successfully processed data
+        processed_case_json_cleaned_foler_path = os.path.join(
+            os.path.dirname(__file__), "..", "..", "data", self.county, "case_json_cleaned", f"result_{dt.today().strftime('%Y-%m-%d.%H:%M:%S.%f')}"
+        )
+        if not os.path.exists(processed_case_json_cleaned_foler_path):
+            # Create the folder if it doesn't exist
+            os.makedirs(processed_case_json_cleaned_foler_path)
+
+        logger = self.configure_logger(processed_case_json_cleaned_foler_path)
 
         #This loads the environment for interacting with CosmosDB #Dan: Should this be moved to the .env file?
         load_dotenv()
@@ -34,19 +46,16 @@ class Updater():
             client = CosmosClient(URL, credential=KEY)
         except Exception as e:
             logger.error(f"Error instantiating CosmosClient: {e.status_code} - {e.message}")
-            # bkj - Need to save list_case_json_files(all input filenames) to a separate file.
             return
         try:
             database = client.get_database_client(DATA_BASE_NAME)
         except Exception as e:
             logger.error(f"Error instantiating DatabaseClient: {e.status_code} - {e.message}")
-            # bkj - Need to save list_case_json_files(all input filenames) to a separate file.
             return
         try:
             COSMOSDB_CONTAINER_CASES_CLEANED = database.get_container_client(CONTAINER_NAME_CLEANED)
         except Exception as e:
             logger.error(f"Error instantiating ContainerClient: {e.status_code} - {e.message}")
-            # bkj - Need to save list_case_json_files(all input filenames) to a separate file.
             return
 
         case_json_cleaned_folder_path = os.path.join(
@@ -56,6 +65,9 @@ class Updater():
 
         for case_json in list_case_json_files:
             in_file = case_json_cleaned_folder_path + "/" + case_json
+            if not os.path.isfile(in_file):
+                continue
+
             with open(in_file, "r") as f:
                 input_dict = json.load(f)
             logger.info(f"[Case Filename: {case_json}, Case Number: {input_dict.get('case_number', None)}, HTML Hash: {input_dict.get('html_hash', None)}]")
@@ -67,7 +79,6 @@ class Updater():
                 cases = list(COSMOSDB_CONTAINER_CASES_CLEANED.query_items(query=hash_query,enable_cross_partition_query=True))
             except Exception as e:
                 logger.error(f"Error querying cases-cleaned database for an existing hash: {e.status_code} - {e.message}")
-                # bkj - Need to save case_json(input filename) to a separate file.
                 continue
 
             if len(cases) > 0:
@@ -82,7 +93,6 @@ class Updater():
                 cases = list(COSMOSDB_CONTAINER_CASES_CLEANED.query_items(query=case_query,enable_cross_partition_query=True))
             except Exception as e:
                 logger.error(f"Error querying cases-cleaned database for an existing cases: {e.status_code} - {e.message}")
-                # bkj - Need to save case_json(input filename) to a separate file.
                 continue
 
             #If there are no cases that match the cause number, then create the case ID, add a version number of 1 to the JSON and push the JSON to the database.
@@ -93,7 +103,6 @@ class Updater():
                 COSMOSDB_CONTAINER_CASES_CLEANED.create_item(body=input_dict)
             except Exception as e:
                 logger.error(f"Error inserting this case to cases-cleaned database: {e.status_code} - {e.message}")
-                # bkj - Need to save case_json(input filename) to a separate file.
                 continue
 
             logger.info(f"Insertion successfully done with id: {input_dict['id']}, version: { input_dict['version']}")
